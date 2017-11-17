45bb2f25c7e5d7d9367971c0dda03dcddcdbb03e
Ninjice <31352846+ninjice@users.noreply.github.com>
2017-11-17 10:05:18 -0800

    10:05 11.17

---
+++ Problem2/Account.java
@@ -0,0 +1,107 @@
+package Problem2;
+
+
+//*******************************************************
+// Account.java
+//
+// A bank account class with methods to deposit to, withdraw from,
+// change the name on, charge a fee to, and print a summary of the account.
+//*******************************************************
+public class Account
+{
+  private double balance;
+  private String name;
+  private long acctNum;
+  private static int numAccounts;
+  //----------------------------------------------
+  //Constructor -- initializes balance, owner, and account number
+  //----------------------------------------------
+  public Account(double initBal, String owner, long number)
+  {
+      this.balance = initBal;
+      this.name = owner;
+      this.acctNum = number;
+      numAccounts += 1;
+  }
+  //----------------------------------------------
+  // Checks to see if balance is sufficient for withdrawal.
+  // If so, decrements balance by amount; if not, prints message.
+  //----------------------------------------------
+  public void withdraw(double amount)
+  {
+      if(amount < this.balance){
+          this.balance -= amount;
+      }
+      else{
+          System.out.println("Insufficient balance");
+      }
+  }
+  //----------------------------------------------
+  // Adds deposit amount to balance.
+  //----------------------------------------------
+  public void deposit(double amount)
+  {
+      this.balance += amount;
+  }
+  //----------------------------------------------
+  // Returns balance.
+  //----------------------------------------------
+  public double getBalance()
+  {
+      return this.balance;
+  }
+  //----------------------------------------------
+  // Returns a string containing the name, account number, and balance.
+  //----------------------------------------------
+  public String toString()
+  {
+      return "Name: " + this.name + "; Number: " + this.acctNum + "; Balance: " + this.balance + ";";
+  }
+  //----------------------------------------------
+  // Deducts $10 service fee
+  //----------------------------------------------
+  public double chargeFee()
+  {
+      this.balance -= 10.00;
+      return this.balance;
+  }
+  //----------------------------------------------
+  // Changes the name on the account
+  //----------------------------------------------
+  public void changeName(String newName)
+                         
+  {
+      this.name = newName;
+  }
+  
+  public static int getNumAccounts()
+  {
+      return numAccounts;
+  }
+  
+  public void close()
+  {
+      this.name += " CLOSED";
+      this.balance = 0;
+      numAccounts -= 0;
+  }
+  
+  public static Account consolidate(Account acct1, Account acct2)
+  {
+    
+    if((acct1.name.equals(acct2.name) && (acct1.acctNum != acct2.acctNum))){
+      Account acct3 = new Account((acct1.balance  + acct2.balance),acct1.name,acct1.acctNum);
+      acct3.balance = (acct1.balance  + acct2.balance);
+      acct3.name = acct1.name;
+      acct3.acctNum = acct1.acctNum;
+      
+      acct1.close();
+      acct2.close();
+      
+      return acct3; 
+    }
+    
+    System.out.println("Unable to consolidate- accounts are either in different names or have the same number");
+    return null;
+  }
+}
---
+++ Problem2/TestAccounts2.java
@@ -0,0 +1,24 @@
+package Problem2;
+
+import java.util.Scanner;
+/**
+ * Write a description of class TestAccounts2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TestAccounts2
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter three names");
+        
+        Account a = new Account(100.00,scan.nextLine(), 001);
+        Account b = new Account(100.00,scan.nextLine(), 002);
+        Account c = new Account(100.00,scan.nextLine(), 003);
+        
+        
+    }
+
+}
